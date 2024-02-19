ff312f03726a79414584cd721c86caac752d9d7c
Karthik kumar reddy <88179147+Karthik-sanju@users.noreply.github.com>
2024-02-19 13:52:06 +0530

    Add files via upload

---
+++ jenkins.sh
@@ -0,0 +1,7 @@
+amazon-linux-extras install java-openjdk17 -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl start jenkins
+systemctl status jenkins
+cat var/lib/jenkins/secrets/initalAdminPassword
